a4beddb99bb6a1c5e49cd7bab322f8b01042ed95
adilsuccessive <mohammad.adil@successive.tech>
2019-01-16 18:51:49 +0530

    fix:fix linting erroe

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -1,33 +1,26 @@
-function diamond(n){
-    let a='';
+function diamond(n) {
+  let a = "", i, j, k;
 
-    for(let i=0; i<n; i++)
-    {
-     
-     for(let j=0; j<n-i-1; j++)
-     {
-        a += ' ';
-     } 
-     for(let k=0; k<=i; k++)
-     {
-         a += '* ';
-     }
-     
-    a += '\n';  
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      a += " ";
     }
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<i; j++)
-        {
-            a += ' ';
-        }
-        for(k=n; k>i; k--)
-        {
-            a += '* ';
-        }
-    a += '\n';    
+    for (k = 0; k <= i; k++) {
+      a += "* ";
     }
-console.log(a)
+
+    a += "\n";
+  }
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < i; j++) {
+      a += " ";
+    }
+    for (k = n; k > i; k--) {
+      a += "* ";
+    }
+    a += "\n";
+  }
+  console.log(a);
 }
 diamond(5);
 diamond(3);
--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,21 +1,18 @@
-function equilateral(n){
-    let a='';
+function equilateral(n) {
+  let a = "", i, j, k;
 
-    for(let i=0; i<n; i++)
-    {
-     
-     for(let j=0; j<n-i-1; j++)
-     {
-        a += ' ';
-     } 
-     for(let k=0; k<=i; k++)
-     {
-         a += '* ';
-     }
-     
-    a += '\n';  
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      a += " ";
     }
-console.log(a)
+    
+    for (k = 0; k <= i; k++) {
+      a += "* ";
+    }
+
+    a += "\n";
+  }
+  console.log(a);
 }
 equilateral(5);
 equilateral(3);
